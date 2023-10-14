1f7d6861d7d474a9ea2812a75b4576e93074e1aa
Pedro Guzman <my.rg.developer@gmail.com>
2023-10-14 08:43:04 -0600

    Access

--- system/conexion.php
+++ system/conexion.php
@@ -4,8 +4,8 @@
 
         private $host  = "127.0.0.1";// "34.130.137.64"; 
         private $bd    = "rpas";
-        private $user  = "eltruekecom_pg";
-        private $pass  = "trueke2021!";//"C0ll3ct42021!";
+        private $user  = "rpas";
+        private $pass  = "rpas2023!";//"C0ll3ct42021!";
         protected $con = null;
 
         // Se establece la conexion a la base de datos
