06b837dcd188f0ad085b5bb39ab0b90e37481338
Pedro Guzman <my.rg.developer@gmail.com>
2023-10-14 09:03:01 -0600

    Add view app.html

--- system/hospedajes.php
+++ system/hospedajes.php
@@ -8,7 +8,11 @@ class Hospedajes extends Conexion {
 public function getHospedajes()
 {
 
-    $query = $this->con->prepare("SELECT * FROM hospedajes 
+    $query = $this->con->prepare("
+        select fecha, hora, hoteles.nombre, destinos.destino, tipos.tipo, costo from hospedajes
+        inner join hoteles on hoteles.id = hospedajes.hotel_id
+        inner join destinos on destinos.id = hospedajes.destino_id
+        inner join tipos on tipos.id = hospedajes.tipo_id;
     ");
 
     $query->execute([]);
